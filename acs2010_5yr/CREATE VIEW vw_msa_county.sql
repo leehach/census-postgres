20d4b76e4529df6999ea34686d271eb0fec3c9b2
Lee Hachadoorian <Lee.Hachadoorian@gmail.com>
2014-05-15 16:13:57 -0400

    Added single view which returns all tracts in MSAs with associated MSA code and name.

---
+++ acs2010_5yr/CREATE VIEW vw_msa_county.sql
@@ -0,0 +1,11 @@
+﻿DROP VIEW IF EXISTS acs2010_5yr.vw_msa_county;
+CREATE VIEW acs2010_5yr.vw_msa_county AS
+select stusab, name AS cbsa_name, state || county AS fips, cbsa,
+	CASE WHEN name like '%Metro Area%' THEN 'metro'
+		WHEN NAME LIKE '%Micro Area%' THEN 'micro'
+		ELSE NULL
+	END AS metro_micro
+from acs2010_5yr.geoheader where sumlevel = 322
+;
+
+--SELECT * FROM acs2010_5yr.geoheader WHERE sumlevel = 320 and name ilike 'New York%'
